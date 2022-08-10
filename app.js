ae8345b54176c23b7274b1566afd73032b7c00cd
Sbadino <badino.santi@gmail.com>
2022-08-10 09:56:39 -0300

    Calling each API_NAME

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/*
+
+Calling each API_NAME:
+
+  *accounts assets customers datapoints devices documents forms invites
+  *media messages namespaces orders patients relationships rules
+  *templates users workflows
+
+*/
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -23,8 +33,216 @@ app.get("/accounts", (req, res) => {
     })
 })
 
+app.get("/assets", (req, res) => {
+  axios.get("https://api.factoryfour.com/assets/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/customers", (req, res) => {
+  axios.get("https://api.factoryfour.com/customers/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/datapoints", (req, res) => {
+  axios.get("https://api.factoryfour.com/datapoints/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/devices", (req, res) => {
+  axios.get("https://api.factoryfour.com/devices/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/documents", (req, res) => {
+  axios.get("https://api.factoryfour.com/documents/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/forms", (req, res) => {
+  axios.get("https://api.factoryfour.com/forms/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
 app.get("/invites", (req, res) => {
   axios.get("https://api.factoryfour.com/invites/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/media", (req, res) => {
+  axios.get("https://api.factoryfour.com/media/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/messages", (req, res) => {
+  axios.get("https://api.factoryfour.com/messages/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/namespaces", (req, res) => {
+  axios.get("https://api.factoryfour.com/namespaces/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/orders", (req, res) => {
+  axios.get("https://api.factoryfour.com/orders/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/patients", (req, res) => {
+  axios.get("https://api.factoryfour.com/patients/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/relationships", (req, res) => {
+  axios.get("https://api.factoryfour.com/relationships/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/rules", (req, res) => {
+  axios.get("https://api.factoryfour.com/rules/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/templates", (req, res) => {
+  axios.get("https://api.factoryfour.com/templates/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/users", (req, res) => {
+  axios.get("https://api.factoryfour.com/users/health/status")
+  .then((fetchRes) => {
+    res.json(fetchRes.data);
+  })
+  .catch((err) => {
+    res.json({ 
+      success: false,
+      message: err.message,
+    });
+  })
+})
+
+app.get("/workflows", (req, res) => {
+  axios.get("https://api.factoryfour.com/workflows/health/status")
   .then((fetchRes) => {
     res.json(fetchRes.data);
   })
