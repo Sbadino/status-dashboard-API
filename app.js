197a6346c119f9c241bacf525911545f5f88ffe8
Sbadino <badino.santi@gmail.com>
2022-08-09 21:07:06 -0300

    API commit

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// const http = require('http');
-// const port = 3000
-
-// const server = http.createServer(function(req, res) {
-//     Response.write('Hello Node');
-//     res.end();
-// })
-
-// server.listen(port, function(error) {
-//     if(error) {
-//         console.log('Not working.', error);
-//     } else {
-//         console.log('Server running on port ' + port);
-//     }
-// })
 const fetch = require("node-fetch");
 const express = require('express');
 const app = express();
@@ -26,12 +11,14 @@ app.listen(process.env.PORT || 3000, () => {
 app.use(cors());
 
 app.get("/", (req, res) => {
-    
     axios.get("https://api.factoryfour.com/API_NAME/health/status")
     .then((fetchRes) => {
       res.json(fetchRes.data);
     })
     .catch((err) => {
-      console.log(err);
+      res.json({ 
+        success: false,
+        message: err.message,
+      });
     })
 })
